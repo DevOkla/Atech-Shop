0d34d4b69ef540c7af646d43c4896c8f7ce05660
Maamoun Okla <maamoun.okla@icloud.com>
2023-08-30 09:50:59 +0200

    update

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import { LinkContainer } from "react-router-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>ATech</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <NavbarCollapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign in
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </NavbarCollapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
